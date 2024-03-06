4d859dfd2a5c509230ee1c8bd7d3bb774d0bd87b
Jimmy <jimmyhui92012@gmail.com>
2024-03-05 23:40:34 -0800

    finish repair equipment page

--- my-app/src/Pages/RepairEquipmentPage/RepairEquipmentPage.jsx
+++ my-app/src/Pages/RepairEquipmentPage/RepairEquipmentPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './RepairEquipmentPage.css'
 
 const RepairEquipmentPage = () => {
   return (
-    <div>
-      <h1>Repair Equipment Page</h1>
-      {/* Add your component content here */}
+    <div className='container'>
+      <div className="imageContainer">
+        <img src='./1.png'></img>
+      </div>
+      <div className="imageContainer">
+        <img src='./2.png'></img>
+      </div>
+      <div className="imageContainer">
+        <img src='./3.png'></img>
+      </div>
+      <div className="imageContainer">
+        <img src='./4.png'></img>
+      </div>
     </div>
   );
 };
